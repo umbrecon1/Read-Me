75e89e83e5d026ae244005ac143253c4ef5ecfd6
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-05-21 18:10:54 -0700

    createreadme function)

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
-
+const inq = require('inquirer');
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project?',
+    }
+
+
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function createReadme(answers) {
+    fs.writeFileSync('./README.md', `# ${answers.title}`)
+}
+
+inq
+.prompt(questions)
+.then((answers) => {
+    createReadme(answers)
+})
+.catch((error) => {
+    if (error.isTtyError) {
+        console.error('Prompts could not be rendered in current environment')
+    } else {
+        console.error(`Something went wrong!`, error)
+    }
+})
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+createReadme('a')
